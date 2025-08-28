ee29096ca7f5fda732ebb6809b5e92d041275d2b
mrheadroom <the@mrheadroom.com>
2025-08-28 09:57:58 -0400

    code - crt fixes

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
+import './styles/crt-fixes.css'; // Import CRT effect fixes
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
